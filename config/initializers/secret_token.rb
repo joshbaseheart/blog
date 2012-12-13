363a6efbc72e44627d19147061014297ebc03ac5
Josh Baseheart <josh.baseheart@gmail.com>
2012-12-12 21:09:22 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '83cd398bf7030365ef8ade2b7d2317b0a988c1d81c188155d68cc47ea6446732b0b81253559ee8292a2eb87758f6c5ff21505bd742d55d1ac96e8d502034f20e'
